a64a96bd5b34a62ead1a2b2241a2c8f8654d5f7b
shahriar <shahriarksaurov@gmail.com>
2024-07-28 21:57:28 +0200

    orders and tables

--- controllers/orderControllers.go
+++ controllers/orderControllers.go
@@ -3,11 +3,13 @@ package controllers
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/ShahSau/culinary-bliss/database"
 	"github.com/ShahSau/culinary-bliss/models"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -37,15 +39,94 @@ func GetOrders(c *gin.Context) {
 }
 
 func GetOrder(c *gin.Context) {
-	fmt.Println("GetOrder")
+	order_id := c.Param("id")
+
+	if err := c.ShouldBindJSON(&order_id); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var order models.Order
+
+	defer c.Request.Body.Close()
+
+	err := orderCollection.FindOne(c.Request.Context(), bson.M{"order_id": order_id}).Decode(&order)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"error": nil, "message": "Order retrived successfully", "data": order, "status": http.StatusOK, "success": true})
+
 }
 
 func CreateOrder(c *gin.Context) {
-	fmt.Println("CreateOrder")
+	var orderReq models.Order
+	var table models.Table
+
+	if err := c.ShouldBindJSON(&orderReq); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if orderReq.Table_id == "" {
+		err := tableCollection.FindOne(c.Request.Context(), bson.M{"table_id": orderReq.Table_id}).Decode(&table)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+	}
+
+	orderReq.CreatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	orderReq.UpdatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+
+	orderReq.ID = primitive.NewObjectID()
+	orderReq.Order_id = orderReq.ID.Hex()
+
+	_, err := orderCollection.InsertOne(c.Request.Context(), orderReq)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"error": nil, "message": "Order created successfully", "data": orderReq, "status": http.StatusCreated, "success": true})
 }
 
 func UpdateOrder(c *gin.Context) {
-	fmt.Println("UpdateOrder")
+	var reqOrder models.Order
+	var Table models.Table
+
+	var updateObj primitive.D
+
+	orderId := c.Param("id")
+	if err := c.ShouldBindJSON(&reqOrder); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if reqOrder.Table_id != "" {
+		err := menuCollection.FindOne(c.Request.Context(), bson.M{"table_id": reqOrder.Table_id}).Decode(&Table)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		updateObj = append(updateObj, bson.E{Key: "table_id", Value: reqOrder.Table_id})
+	}
+
+	reqOrder.UpdatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+
+	_, err := orderCollection.UpdateOne(c.Request.Context(), bson.M{"order_id": orderId}, bson.D{{Key: "$set", Value: updateObj}})
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"error": nil, "message": "Order updated successfully", "status": http.StatusOK, "success": true})
+
 }
 
 func DeleteOrder(c *gin.Context) {
--- controllers/tableControllers.go
+++ controllers/tableControllers.go
@@ -2,28 +2,122 @@ package controllers
 
 import (
 	"fmt"
+	"net/http"
+	"time"
 
 	"github.com/ShahSau/culinary-bliss/database"
+	"github.com/ShahSau/culinary-bliss/models"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 var tableCollection *mongo.Collection = database.GetCollection(database.DB, "tables")
 
 func GetTables(c *gin.Context) {
-	fmt.Println("GetTables")
+	tables, err := tableCollection.Find(c.Request.Context(), bson.M{}, nil)
+
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	defer tables.Close(c.Request.Context())
+
+	var results []models.Table
+
+	for tables.Next(c.Request.Context()) {
+		var table models.Table
+		if err = tables.Decode(&table); err != nil {
+			c.JSON(500, gin.H{"error": true, "message": err.Error()})
+		}
+		results = append(results, table)
+	}
+
+	c.JSON(http.StatusOK, gin.H{"error": nil, "message": "Table retrived successfully", "data": results, "status": http.StatusOK, "success": true})
 }
 
 func GetTable(c *gin.Context) {
-	fmt.Println("GetTable")
+	table_id := c.Param("id")
+
+	if err := c.ShouldBindJSON(&table_id); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var table models.Table
+
+	defer c.Request.Body.Close()
+
+	err := tableCollection.FindOne(c.Request.Context(), bson.M{"table_id": table_id}).Decode(&table)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"error": nil, "message": "Table retrived successfully", "data": table, "status": http.StatusOK, "success": true})
 }
 
 func CreateTable(c *gin.Context) {
-	fmt.Println("CreateTable")
+	var tableReq models.Table
+
+	if err := c.ShouldBindJSON(&tableReq); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	defer c.Request.Body.Close()
+
+	tableReq.CreatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	tableReq.UpdatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+
+	tableReq.ID = primitive.NewObjectID()
+	tableReq.Table_id = tableReq.ID.Hex()
+
+	_, err := tableCollection.InsertOne(c.Request.Context(), tableReq)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"error": nil, "message": "Table created successfully", "data": tableReq, "status": http.StatusCreated, "success": true})
+
 }
 
 func UpdateTable(c *gin.Context) {
-	fmt.Println("UpdateTable")
+	var table models.Table
+	table_id := c.Param("id")
+
+	if err := c.ShouldBindJSON(&table); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var updateObj primitive.D
+
+	if table.Number_of_guests != 0 {
+		updateObj = append(updateObj, bson.E{Key: "number_of_guests", Value: table.Number_of_guests})
+	}
+
+	if table.Table_number != 0 {
+		updateObj = append(updateObj, bson.E{Key: "table_number", Value: table.Table_number})
+	}
+
+	table.UpdatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+
+	updateObj = append(updateObj, bson.E{Key: "updated_at", Value: table.UpdatedAt})
+
+	_, err := tableCollection.UpdateOne(c.Request.Context(), bson.M{"table_id": table_id}, bson.D{{Key: "$set", Value: updateObj}})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"error": nil, "message": "Table updated successfully", "status": http.StatusOK, "success": true})
+
 }
 
 func DeleteTable(c *gin.Context) {
