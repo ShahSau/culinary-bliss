dd4ff429220bc13b17a9e58b42ba425c03b1420d
shahriar <shahriarksaurov@gmail.com>
2024-12-24 20:21:22 +0100

    cors

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"os"
 
+	"time"
+
 	"github.com/ShahSau/culinary-bliss/database"
 	docs "github.com/ShahSau/culinary-bliss/docs"
 	"github.com/ShahSau/culinary-bliss/middleware"
 	"github.com/ShahSau/culinary-bliss/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -21,6 +24,15 @@ func main() {
 	database.ConnectDB()
 
 	router := gin.Default()
+	// CORS
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://culinary-bliss.onrender.com", "http://localhost:3000", "http://localhost:8080"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
 
@@ -30,7 +42,8 @@ func main() {
 	docs.SwaggerInfo.Title = "Culinary Bliss API"
 	docs.SwaggerInfo.Description = "Culinary Bliss restaurant management app designed to streamline your operations and elevate your dining experience and for efficient, effective, and effortless restaurant management."
 	docs.SwaggerInfo.Version = "1.0"
-	docs.SwaggerInfo.Host = "https://culinary-bliss.onrender.com"
+	docs.SwaggerInfo.Schemes = []string{"https"}
+	docs.SwaggerInfo.Host = "culinary-bliss.onrender.com"
 	//docs.SwaggerInfo.Host = "localhost:8080"
 
 	routes.AuthRoutes(router)
